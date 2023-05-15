4395ba617d3e8a440fae4db9276bbc4ec0554d80
Andrey Chertykovtsev <chertiav@gmail.com>
2023-05-15 12:28:41 +0200

    adding send mail logic

--- components/modules/FeedBackForm/FeedBackForm.tsx
+++ components/modules/FeedBackForm/FeedBackForm.tsx
@@ -1,13 +1,17 @@
+import { MutableRefObject, useRef, useState } from 'react';
 import { useStore } from 'effector-react';
 import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-toastify';
 //=============================================
 import { $mode } from '@/context/mode';
 import NameInput from '@/components/modules/FeedBackForm/NameInput';
-import styles from '@/styles/feedbackForm/index.module.scss';
-import { FeedbackInputs } from '@/types/types/feedbackForm';
+import MessageInput from '@/components/modules/FeedBackForm/MessageInput';
 import PhoneInput from '@/components/modules/FeedBackForm/PhoneInput';
 import EmailInput from '@/components/modules/FeedBackForm/EmailInput';
-import MessageInput from '@/components/modules/FeedBackForm/MessageInput';
+import { FeedbackInputs } from '@/types/types/feedbackForm';
+import styles from '@/styles/feedbackForm/index.module.scss';
+import spinnerStyles from '@/styles/spinner/index.module.scss';
 
 const FeedBackForm = () => {
 	const mode = useStore($mode);
@@ -17,9 +21,27 @@ const FeedBackForm = () => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm<FeedbackInputs>();
+	const [spinner, setSpinner] = useState(false);
+	const formRef = useRef() as MutableRefObject<HTMLFormElement>;
 
-	const submitForm = (data: FeedbackInputs) => {
-		console.log(data);
+	const submitForm = () => {
+		setSpinner(true);
+		emailjs
+			.sendForm(
+				'service_l8g280o',
+				'template_b07dbab',
+				formRef.current,
+				'WGzMJhGp9AlRXRTnz'
+			)
+			.then((result) => {
+				setSpinner(false);
+				toast.success(`Сообщение отправлено! ${result.text}`);
+			})
+			.catch((error) => {
+				setSpinner(false);
+				toast.error(`Что-то пошло не так! ${error.text}`);
+			});
+		formRef.current.reset();
 	};
 
 	return (
@@ -30,6 +52,7 @@ const FeedBackForm = () => {
 			<form
 				className={styles.feedback_form__form}
 				onSubmit={handleSubmit(submitForm)}
+				ref={formRef}
 			>
 				<NameInput
 					register={register}
@@ -52,7 +75,16 @@ const FeedBackForm = () => {
 					darkModeClass={darkModeClass}
 				/>
 				<div className={styles.feedback_form__form__btn}>
-					<button>Отправить сообщение</button>
+					<button>
+						{spinner ? (
+							<span
+								className={spinnerStyles.spinner}
+								style={{ top: '6px', left: '47%' }}
+							/>
+						) : (
+							'Отправить сообщение'
+						)}
+					</button>
 				</div>
 			</form>
 		</div>
